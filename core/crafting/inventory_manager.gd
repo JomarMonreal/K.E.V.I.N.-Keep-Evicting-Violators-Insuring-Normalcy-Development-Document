b45c9a7687a61577a951ccd3a3480dccdbc9946d
DvaeFroot <ddjimenez2@up.edu.ph>
2025-12-31 00:17:18 +0800

    basic inventory system

---
+++ core/crafting/inventory_manager.gd
@@ -0,0 +1,82 @@
+class_name InventoryManager
+extends Control
+
+var Items: Array[Item] = []
+var Slots: Array[InventorySlot] = []
+@export var max_stack: int = 20
+
+func _ready() -> void:
+	for child in $InventoryRow1.get_children():
+		if child is InventorySlot:
+			child.InventoryManagerInstance = self
+			Slots.append(child)
+
+# MAIN METHODS
+
+func store_item(item: Item, count: int) -> void:
+	var slot = get_item_stack_for_storing(item)
+	
+	if slot != null:
+		slot.add_to_stack(count)
+		return
+
+	slot = get_free_slot()
+	if slot == null:
+		return
+
+	slot.set_item(item, count)
+
+
+func less_item(item: Item, count: int) -> void:
+	var slot = get_item_stack_for_removing(item)	
+
+	if slot == null:
+		print("No item stack found for " + item.item_name)
+		return
+
+	if count > slot.item_count:
+		print("Not enough items")
+		return
+
+	slot.sub_from_stack(count)
+
+# Coroutines
+
+func get_free_slot() -> InventorySlot:
+	for slot in Slots:
+		if slot.is_empty:
+			return slot
+	return null
+
+
+func get_item_stack_for_storing(item: Item) -> InventorySlot:
+	for slot in Slots:
+		if slot.is_empty:
+			continue
+		if slot.item.item_name == item.item_name:
+			if slot.item_count != max_stack:
+				return slot
+	return null
+
+
+func get_item_stack_for_removing(item: Item) -> InventorySlot:
+	for slot in Slots:
+		if slot.is_empty:
+			continue
+		if slot.item.item_name == item.item_name:
+			return slot
+	return null
+
+
+# DEBUG
+
+func _on_button_pressed() -> void:
+	var item_resource: Item = load("res://resources/items/materials/sample_item_1.tres")
+	print("storing item")
+	store_item(item_resource, 10)
+
+
+func _on_button_2_pressed() -> void:
+	var item_resource: Item = load("res://resources/items/materials/sample_item_1.tres")
+	print("removing item")
+	less_item(item_resource, 10)
---
+++ core/crafting/inventory_slot.gd
@@ -0,0 +1,78 @@
+class_name InventorySlot
+extends PanelContainer
+
+var InventoryManagerInstance: InventoryManager = null
+var item: Item = null
+
+var is_mouse_inside: bool = false
+
+@onready var ItemTexture = $MarginContainer/VBoxContainer/TextureRect
+@onready var ItemCount = $MarginContainer/VBoxContainer/TextureRect/TextEdit
+var item_count = 0
+var is_empty = true
+
+func _ready() -> void:
+	clear_item()
+
+func _process(_delta: float) -> void:
+	if is_mouse_inside:
+		if Input.is_action_just_pressed("ui_left_click"):
+			CursorManager.slot_source = self
+		if Input.is_action_just_released("ui_left_click"):
+			CursorManager.slot_dest = self
+
+
+func set_item(new_item: Item, count) -> void:
+	if new_item == null:
+		item = null
+		item_count = 0
+		ItemTexture.texture = null
+		ItemCount.text = ""
+		is_empty = true
+		return
+	item = new_item
+	print(item.item_name)
+	ItemTexture.texture = item.texture
+	item_count = count
+	if item_count > InventoryManagerInstance.max_stack:
+		item_count = InventoryManagerInstance.max_stack
+	ItemCount.text = "x" + str(item_count)
+	is_empty = false
+
+
+func add_to_stack(count) -> void:
+	item_count += count
+	if item_count > InventoryManagerInstance.max_stack:
+		item_count = InventoryManagerInstance.max_stack
+	ItemCount.text = "x" + str(item_count)
+
+
+func sub_from_stack(count) -> void:
+	item_count -= count
+	if item_count == 0:
+		clear_item()
+		return
+		
+	ItemCount.text = "x" + str(item_count)
+
+
+func clear_item() -> void:
+	item_count = 0
+	item = null
+	ItemTexture.texture = null
+	ItemCount.text = ""
+	is_empty = true
+
+
+func _on_mouse_exited() -> void:
+	is_mouse_inside = false
+	if not CursorManager.is_dragging:
+		CursorManager.clear_tooltip()
+
+func _on_mouse_entered() -> void:
+	is_mouse_inside = true
+	if item != null:
+		if CursorManager.is_dragging:
+			CursorManager.set_buffer_item(item)
+			return
+		CursorManager.set_tooltip(item)
