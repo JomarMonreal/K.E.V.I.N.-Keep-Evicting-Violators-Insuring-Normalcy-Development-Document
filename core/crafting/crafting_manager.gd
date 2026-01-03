153747c51a7f07fff1d8bc96ef2fdcf008079188
JomarMonreal <jpmonreal2035@gmail.com>
2026-01-03 10:45:46 +0800

    fix: switch camera using Y

--- core/crafting/crafting_manager.gd
+++ core/crafting/crafting_manager.gd
@@ -1,6 +1,2 @@
 class_name CraftingManager
 extends Node2D
-
-
-
-
--- entities/invader/states/invader_moving.gd
+++ entities/invader/states/invader_moving.gd
@@ -6,6 +6,7 @@ func physics_process(delta: float) -> int:
 	invader.global_position = invader.global_position.move_toward(invader.target_node.global_position, invader.speed * delta)
 	if invader.global_position.distance_to(invader.target_node.global_position) < 1.0:
 		invader.current_node = invader.target_node
-		invader.target_node = invader.target_node.neighbors.pick_random()
+		var target_neighbors = invader.target_node.neighbors.filter(func(neighbor): return neighbor != invader.current_node)
+		invader.target_node = target_neighbors.pick_random()
 	return InvaderBaseState.State.Moving
 	
