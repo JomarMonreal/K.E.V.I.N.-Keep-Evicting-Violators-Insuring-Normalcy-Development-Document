0a98a46c440e5d6f83fb4b83c274e0f0852d1918
clenelpogi <uplbgds@gmail.com>
2026-01-05 17:30:05 +0800

    ADDED: global autoload and game_manager node

--- core/game_manager/game_manager.gd
+++ core/game_manager/game_manager.gd
@@ -1,11 +1,67 @@
+class_name GameManager
 extends Node
 
+enum GameCondition {
+	VICTORY,
+	KILLED,
+	INSANE,
+}
 
-# Called when the node enters the scene tree for the first time.
+const MAX_NIGHTS : int = 7
+@onready var current_night : int = 0
+
+@export var world_scene : Node2D
+var current_scene: Node2D
+#var current_ui_scene: Control
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	Global.game_manager = self
+	
+	EventListener.player_killed.connect(_on_player_killed)
+	EventListener.insanity_reached.connect(_on_player_insane)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func change_scene(new_scene: String, delete: bool = true, keep_running: bool = false):
+	if current_scene:
+		if delete:
+			current_scene.queue_free() #delete scene in memory
+		elif keep_running:
+			current_scene.visible = false #hide scene but keep in memory
+		else:
+			remove_child(current_scene) #sumunod lang ako sa tutorial idk what this is
+	
+	var new = load(new_scene).instantiate()
+	add_child(new)
+	current_scene = new
+
+
+func start_game():
 	pass
+
+
+func end_game(condition: GameCondition):
+	if not condition:
+		return
+	
+	if condition == GameCondition.VICTORY:
+		pass
+	elif condition == GameCondition.KILLED:
+		pass
+	elif condition == GameCondition.INSANE:
+		pass
+
+
+func advance_night():
+	current_night += 1
+	
+	if current_night > MAX_NIGHTS:
+		end_game(GameCondition.VICTORY)
+
+
+func _on_player_killed():
+	end_game(GameCondition.KILLED)
+
+
+func _on_player_insane():
+	end_game(GameCondition.INSANE)
