edf75ff32593678dc34aa85d754865b5e49080cc
JomarMonreal <jpmonreal2035@gmail.com>
2026-01-11 10:08:11 +0800

    Enhance asset import settings and UI updates for night management
    - Updated PNG and SVG import settings across various assets to improve compression and color channel remapping.
    - Added a timer UI element to the night start scene for better player feedback during planning and invading phases.
    - Modified night management logic to include a new timer for displaying remaining time during the invasion.
    - Adjusted node structure in the night start scene for improved organization and clarity.
    - Updated project configuration to use Godot 4.5 features.

--- core/global.gd
+++ core/global.gd
@@ -1,5 +1,5 @@
 extends Node
 
 var game_manager : GameManager
-
+var current_night: int = 1
 var main_menu_file : String = "res://core/ui/main_menu.tscn"
--- core/night_manager/night_manager.gd
+++ core/night_manager/night_manager.gd
@@ -9,8 +9,10 @@ class_name NightManager
 @onready var planning_timer: Timer = $PlanningTimer
 @onready var plan_to_invade_timer: Timer = $PlanToInvadeTimer
 @onready var invading_timer: Timer = $InvadingTimer
-@onready var night_start_layer: CanvasLayer = $Camera2D/NightStart/CanvasLayer
-@onready var night_panel_label: Label = $Camera2D/NightStart/CanvasLayer/Label
+@onready var night_start_layer: CanvasLayer = $Camera2D/NightStart/Screen
+@onready var night_label: Label = $Camera2D/NightStart/Screen/Label
+@onready var ui_timer_layer: CanvasLayer = $Camera2D/NightStart/Timer
+@onready var ui_label_label: Label = $Camera2D/NightStart/Timer/Panel/Label
 
 signal has_started_planning
 signal has_ended_planning
@@ -20,19 +22,33 @@ signal has_ended_invading
 # Called when the node enters "res://assets/main_assets/Map/Map_Planning.png"the scene tree for the first time.
 func _ready() -> void:
 	path_graph.background_sprite.texture = planning_background
-	night_panel_label.text = 'Night ' + str(Global.current_night) 
+	night_label.text = 'Night ' + str(Global.current_night) 
+	night_start_layer.visible = true
+	ui_timer_layer.visible = false
 	loading_timer.start()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	var t_left := 0.0
+
+	if ui_timer_layer.visible:
+		if not planning_timer.is_stopped():
+			t_left = planning_timer.time_left
+		elif not invading_timer.is_stopped():
+			t_left = invading_timer.time_left
+
+	var total := int(ceil(max(t_left, 0.0)))
+	var mins := total / 60
+	var secs := total % 60
+	ui_label_label.text = "%02d:%02d" % [mins, secs]
 
 
 func _on_loading_timer_timeout() -> void:
 	has_started_planning.emit()
 	night_start_layer.visible = false
+	ui_timer_layer.visible = true
 	loading_timer.stop()
 	planning_timer.start()
 	pass # Replace with function body.
@@ -41,8 +57,9 @@ func _on_loading_timer_timeout() -> void:
 func _on_planning_timer_timeout() -> void:
 	has_ended_planning.emit()
 	planning_timer.stop()
-	night_panel_label.text = 'The invaders are now approaching...'
+	night_label.text = 'The invaders are now approaching...'
 	night_start_layer.visible = true
+	ui_timer_layer.visible = false
 	plan_to_invade_timer.start()
 	pass # Replace with function body.
 
@@ -51,6 +68,16 @@ func _on_plan_to_invade_timer_timeout() -> void:
 	has_started_invading.emit()
 	path_graph.background_sprite.texture = invading_background
 	night_start_layer.visible = false
+	ui_timer_layer.visible = true
 	plan_to_invade_timer.stop()
 	invading_timer.start()
+	pass # Replace with function body.
+
+
+func _on_invading_timer_timeout() -> void:
+	has_ended_invading.emit()
+	night_label.text = 'Kevin survived the night!'
+	night_start_layer.visible = true
+	ui_timer_layer.visible = false
+	invading_timer.stop()
 	pass # Replace with function body.
