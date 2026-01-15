47363e60d5e9ecf0fe825d94cfe084529c00809e
clenelpogi <uplbgds@gmail.com>
2026-01-15 18:37:34 +0800

    refactor(player sanity component): prep for signal use

--- entities/player/sanity_component.gd
+++ entities/player/sanity_component.gd
@@ -1,7 +1,6 @@
 extends Node
 
 @export var max_sanity : float = 100
-@export var sanity_increment : float = 10
 var sanity : float = 0
 
 
@@ -10,12 +9,16 @@ func _ready() -> void:
 	EventListener.insanity_reached.connect(_on_insanity_reached)
 
 
-func _on_insanity_increased():
-	sanity += sanity_increment
+func damage_sanity(sanity_damage: float):
+	sanity += sanity_damage
 	sanity = max(sanity, max_sanity)
 	
 	if sanity >= max_sanity:
 		EventListener.insanity_reached.emit()
+
+
+func _on_insanity_increased(damage: float):
+	damage_sanity(damage)
 
 
 func _on_insanity_reached():
