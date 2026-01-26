2f59cbda8b17b9a069053e21991e40077cb11dde
clenelpogi <uplbgds@gmail.com>
2026-01-26 22:00:24 +0800

    added outline shader in files

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -5,9 +5,11 @@ extends Control
 @export var delay : float = 0.2
 
 @onready var play_button : Button = $Control/Play
+
+
 func _on_play_pressed() -> void:
-	print("play!")
 	AudioManager.create_2d_audio(SoundEffectSettings.SOUND_EFFECT_TYPE.BUTTON_CLICK)
+	await get_tree().create_timer(delay).timeout
 	Global.game_manager.change_ui_scene('')
 	Global.game_manager.change_world_scene(main_scene) #insert scene to night
 	
